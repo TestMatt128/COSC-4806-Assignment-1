267dad1063f9c598f8aa5f93021103268b93bf5c
Matt <mdelfierro@algomau.ca>
2025-05-23 14:16:12 +0000

    Added the logout php and fixed some code

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+
+
+// We first need to check if the user is logged in.
+// If not, automatically redirect them to the login page, fyi; login.php
+?>
+
 <DOCTYPE html>
 <html>
   <head>
@@ -7,7 +16,11 @@
 
     <h1>Assignment 1: Login Page</h1>
 
-    <p><a href="/login.php"> Click this link to login!</a></p>
+    <p> Welcome to the login page, <?=$_SESSION['username'] ?></p>
     
   </body>
+
+  <footer>
+     <p><a href="/logout.php"> Click this link to logout and start again!</a></p>
+  </footer>
 </html>
--- validate.php
+++ validate.php
@@ -5,11 +5,13 @@
   $valid_username = "Matt";
   $valid_password = "TestMatt128";
 
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+  $username = $_REQUEST['username'];
+  $_SESSION['username'] = $username;
+  $password = $_REQUEST['password'];
 
   if ($username == $valid_username && $password == $valid_password) {
-    echo "Login successful! Hello, $username!";
+    $_SESSION['authenticated'] = true;
+    header("Location: /");
   } else {
     if(isset($_SESSION['login_attempt_fails'])){
       $_SESSION['login_attempt_fails'] += 1;
@@ -17,6 +19,8 @@
       $_SESSION['login_attempt_fails'] = 1;
     }
 
+    // After the login fail, we must redirect them to the login page again.
+    // The fail counter will be displayed every time they fail to login and see this message.
     echo "Login failed. Total fails: " . $_SESSION['login_attempt_fails'];
   }
 
